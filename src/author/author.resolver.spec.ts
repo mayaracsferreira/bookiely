521b6e0a4968d13d5d35487baf1824c5852f50dd
Ferreira Mayara <fmayara@lenovo.com>
2023-10-05 12:22:14 -0300

    Add test to author resolver, mock author service

--- src/author/author.resolver.spec.ts
+++ src/author/author.resolver.spec.ts
@@ -1,13 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthorResolver } from './author.resolver';
 import { AuthorService } from './author.service';
+import { Author } from './author.model';
 
 describe('AuthorResolver', () => {
   let resolver: AuthorResolver;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthorResolver, AuthorService]
+      providers: [
+        AuthorResolver,
+        {
+          provide: AuthorService,
+          useFactory: () => ({
+            create: jest.fn((author: Author) => ({
+              id: 1234,
+              name: 'Silvia Federici'
+            })),
+            findOneById: jest.fn((id: number) => ({
+              id: 1234,
+              name: 'Silvia Federici'
+            }))
+          })
+        }
+      ],
     }).compile();
 
     resolver = module.get<AuthorResolver>(AuthorResolver);
@@ -16,4 +32,14 @@ describe('AuthorResolver', () => {
   it('should be defined', () => {
     expect(resolver).toBeDefined();
   });
+
+  it('should find and return an author', async () => {
+    const author = await resolver.author(1234)
+    expect(author).toEqual(
+      {
+        id: 1234,
+        name: 'Silvia Federici'
+      }
+    )
+  })
 });
